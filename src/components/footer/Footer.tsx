3841556a0a2dff1152c5fd45467a018725a1a0af
帅得乱七八糟 <284970670@qq.com>
2021-05-29 21:58:12 +0800

    footer使用行内样式来修改的

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,8 +5,13 @@ import { useTranslation } from 'react-i18next';
 export const Footer: React.FC = () => {
   // 这里的Translation甚至都不需要ts的类型定义吗？
   const { t } = useTranslation();
+  const footerStyle = {
+    marginLeft: 130,
+    marginRight: 130,
+    height: 140
+  };
   return <>
-    <Layout.Footer>
+    <Layout.Footer style={footerStyle}>
       <Typography.Title level={4}>
         {/* 版权所有 @ React 旅游网 */}
         {t("footer.detail")}
